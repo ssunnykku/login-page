8c6b801e9b48ac420f521118db35558a735860f6
ssunnykku <ssunnykku@gmail.com>
2022-08-01 02:42:27 +0900

    DOM을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -8,7 +8,7 @@ const id = document.querySelector("#id"),
 // ejs에서 값이 넘어오기 전에 console.log가 실행되어 Null이 반환됨 -> difer 적용    
 
 loginBtn.addEventListener("click", login);
-
+   
 function login() {
     const req = {
         id: id.value,
