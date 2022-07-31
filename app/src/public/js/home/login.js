721a8e2fefe42ea489bf3cd2649e4b35ed40dcab
ssunnykku <ssunnykku@gmail.com>
2022-08-01 02:26:04 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,5 +1,18 @@
 "use strict";
 
-console.log("hello")
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
 
-console.log("bye")
+// console.log(id)
+// ejs에서 값이 넘어오기 전에 console.log가 실행되어 Null이 반환됨 -> difer 적용
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    };
+    console.log(req);
+};
